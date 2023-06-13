9c984545fd0b9f38ce2e503ddbfd444c53d10086
unknown <francoexe2004@gmail.com>
2023-06-13 00:41:12 -0300

    portafolio

---
+++ js/main.js
@@ -0,0 +1,80 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+//     reset: true
+});
+
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+
+/*===== EMAIL JS =====*/
+
+// (function() {
+// https://dashboard.emailjs.com/admin/account
+// emailjs.init('ZIeMGejx36fQRJ__7');
+// })();
+
+// const contactForm=document.getElementById('contact-form'),
+//     contactMessage=document.getElementById('contact-message')
+
+//     const sendMail = (e) =>{
+//         e.preventDefault()
+//     }
+
+
+//     emailjs.sendForm('service_pu49a0m' ,'template_xvyrj3q', '#contact-form', 'ZIeMGejx36fQRJ__7')
+//     .then(() =>{
+//         contactMessage.textContent= 'Message sent successfully'
+//     }, ()=>{
+//         contactMessage.textContent= 'Message not sent (service error)'
+//     })
+
+//     contactForm.addEventListener('submit, sendEmail')
